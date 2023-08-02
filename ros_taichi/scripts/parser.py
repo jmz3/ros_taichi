0961790b7fd34b233183feaa7a4e25df24ec03f1
JimLiu35 <gliu35@jhu.edu>
2023-08-01 22:10:10 -0400

    minor update, no need to have a __init__.py in urdfpy submodule

--- ros_taichi/scripts/parser.py
+++ ros_taichi/scripts/parser.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 import sys
 # import ros_taichi.third_parties.urdfpy.urdfpy.URDF
-from ..third_parties.urdfpy.urdfpy import URDF
+# from ..third_parties.urdfpy.urdfpy import URDF
+from .. import third_parties
 
 filename = sys.argv[1]
-robot = URDF.load(filename)
+robot = third_parties.URDF.load(filename)
 for l in robot.links:
     if l.taichi is not None:
         print(l.taichi.bodytype.body_type)
--- ros_taichi/third_parties/__init__.py
+++ ros_taichi/third_parties/__init__.py
@@ -1,2 +1,2 @@
 #!/usr/bin/env python3
-
+from .urdfpy.urdfpy import URDF
