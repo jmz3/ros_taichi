e6f9f6c2850fc7c2574b568dcb0a713294d1e5d2
JimLiu35 <gliu35@jhu.edu>
2023-12-23 10:19:27 -0500

    Can change ur5's joint angles.

--- ros_taichi/scripts/parser.py
+++ ros_taichi/scripts/parser.py
@@ -24,15 +24,45 @@ def mesh_loading(file_name):
 
     return vertices, faces, normals
 
+def set_pose(urdf_robot, desired_angles):
+    # This function is for UR5 only
+    # TODO: Need to come up with a general version
+    idx = 0
+    for j in urdf_robot.joints:
+        new_config = j.get_child_pose(desired_angles[idx])
+        j.origin = new_config
+        idx += 1
+        if idx > 5:
+            break
+        # if j.is_valid(new_config):
+        #     j.origin = new_config
+        #     idx += 1
+        # else:
+        #     print("Non-valid Configuration!")
+
+
+
 filename = sys.argv[1]
 robot = third_parties.URDF.load(filename)
 # Get all links
 links = robot.links
-for l in robot.links:
-    if l.taichi is not None:
-        print(l.taichi.bodytype.body_type)
-for m in robot.materials:
-    if m.taichi is not None:
-        print(m.taichi.materialproperty.filename)
-robot.show(use_collision=True)
-# robot.showTemp(use_collision=True)
+joints = robot.joints
+joints_map = robot.joint_map
+
+angle = np.array([0, -np.pi/3, np.pi/2, -2*np.pi/3, -np.pi/2, 0])
+set_pose(robot, angle)
+# angle = [0,0,0,0,np.pi/2,0]
+# print(joints_map)
+# origin_new = joints[1].get_child_pose(angle)
+# print(joints[1].get_child_pose(angle))
+# joints[1].origin = origin_new
+# for j in robot.jonits:
+
+# for l in robot.links:
+#     if l.taichi is not None:
+#         print(l.taichi.bodytype.body_type)
+# for m in robot.materials:
+#     if m.taichi is not None:
+#         print(m.taichi.materialproperty.filename)
+# robot.show()
+robot.showTemp(use_collision=True)
